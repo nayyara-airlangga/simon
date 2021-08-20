7d5972e26054eeff866eea120019786fe8e2f51f
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-08-20 16:30:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# simon
+
+A classic Simon game, or some may refer to it as the Memory Test. This repo is also a part of me trying to learn Javascript and the jQuery library.
---
+++ logic.js
@@ -0,0 +1,88 @@
+let btnColors = ["red", "blue", "green", "yellow"];
+let pattern = [];
+let userPattern = [];
+
+let curLevel = 0;
+let isStarted = false;
+let isGameOver = false;
+
+function restartGame() {
+  curLevel = 0;
+  isStarted = false;
+  pattern = [];
+  isGameOver = false;
+}
+
+$(document).keydown(function () {
+  if (!isStarted) {
+    $("#level-title").text("Level " + curLevel);
+    nextPart();
+    isStarted = true;
+  } else if (isGameOver) {
+    restartGame();
+    $("#level-title").text("Level " + curLevel);
+    nextPart();
+    isStarted = true;
+  }
+});
+
+$(".restart-button").click(function () {
+  restartGame();
+  $("#level-title").text("Level " + curLevel);
+  nextPart();
+  isStarted = true;
+});
+
+$(".btn").click(function () {
+  let userColor = $(this).attr("id");
+  userPattern.push(userColor);
+  playAudio(userColor);
+  clickAnimation(userColor);
+  checkAnswer(userPattern.length - 1);
+});
+
+function playAudio(color) {
+  let audio = new Audio("sounds/" + color + ".mp3");
+  audio.play();
+}
+
+function clickAnimation(curColor) {
+  let curButton = $("#" + curColor);
+  curButton.addClass("pressed");
+  setTimeout(function () {
+    curButton.removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userPattern[currentLevel] === pattern[currentLevel]) {
+    if (userPattern.length === pattern.length) {
+      setTimeout(nextPart, 1000);
+    }
+  } else {
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    isGameOver = true;
+  }
+}
+
+function nextPart() {
+  userPattern = [];
+
+  curLevel++;
+  $("#level-title").text("Level " + curLevel);
+
+  let randomNum = Math.floor(Math.random() * 4);
+  let chosenColor = btnColors[randomNum];
+  pattern.push(chosenColor);
+  $("#" + chosenColor)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playAudio(chosenColor);
+}
